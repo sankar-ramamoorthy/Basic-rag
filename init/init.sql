19776517692b9b906aeb33c297952e58e81e2250
sankar-ramamoorthy <43016553+sankar-ramamoorthy@users.noreply.github.com>
2025-10-17 22:11:56 -0400

    Update init.sql
    deafulat can be serial or gen_random and not both

--- init/init.sql
+++ init/init.sql
@@ -12,7 +12,8 @@ CREATE EXTENSION IF NOT EXISTS vector;
 
 -- Create documents table
 CREATE TABLE IF NOT EXISTS documents (
-  doc_id SERIAL PRIMARY KEY  DEFAULT gen_random_uuid(),
+  --doc_id SERIAL PRIMARY KEY  DEFAULT gen_random_uuid(), -- default can be either serial or gen_random
+  doc_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,  -- Fix here
   text TEXT NOT NULL,
   embedding VECTOR(384),
   source_name TEXT,
